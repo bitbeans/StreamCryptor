037dc33230e1a6bf3bddcd4ceab568a2ce111f71
Christian Hermann <c.hermann@bitbeans.de>
2014-09-17 08:56:28 +0200

    Travis

--- travis-linux-deps.sh
+++ travis-linux-deps.sh
@@ -4,5 +4,5 @@ set -ev
 sudo add-apt-repository ppa:directhex/monoxide -y && sudo apt-get update -qq -y
 sudo apt-get install mono-devel -qq -y
 mozroots --import --sync
-mono --runtime=v4.5 ./.nuget/NuGet.exe restore ./StreamCryptor.sln
-echo 'mono --runtime=v4.5 ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
+mono --runtime=v4.0 ./.nuget/NuGet.exe restore ./StreamCryptor.sln
+echo 'mono --runtime=v4.0 ./packages/NUnit.Runners.2.6.3/tools/nunit-console.exe "$@"' > nunit-console.sh
