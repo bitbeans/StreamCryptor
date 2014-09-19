8d2ce437260ef142f7faca76313696ef50fa0142
Christian Hermann <c.hermann@bitbeans.de>
2014-09-19 19:28:16 +0200

    Travis again

--- travis-build-libsodium.sh
+++ travis-build-libsodium.sh
@@ -5,10 +5,10 @@ git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
 git checkout tags/0.7.0
 ./autogen.sh
-#if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then 
-export CFLAGS="-Os -m32 -arch i386";
-export LDFLAGS="-m32 -arch i386";
-#fi
+if [[ $TRAVIS_OS_UNAME = 'Darwin' ]]; then 
+  export CFLAGS="-Os -m32 -arch i386";
+  export LDFLAGS="-m32 -arch i386";
+fi
 ./configure
 make && sudo make install
 if [[ $TRAVIS_OS_UNAME = 'Linux' ]]; then sudo ldconfig; fi
